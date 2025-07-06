621aae25f214c88606677f74d77b3acf22699dd5
Bhavana <bhavanamunagapati28@gmail.com>
2025-07-06 12:44:23 +0530

    Update and rename CC-1 Number Mirror to CC-1 Number Mirror.c

---
+++ Week-1/Day-1/CC-1 Number Mirror.c
@@ -0,0 +1,24 @@
+Number Mirror
+Write a program that takes a number 
+N
+N as the input, and prints it to the output.
+
+Input Format
+The only line of input contains a single integer.
+
+Output Format
+Output the answer in a single line.
+
+#include <stdio.h>
+
+int main(void) {
+	int n;
+	
+	// Do not print anything before taking input
+	scanf("%d",&n);
+	printf("%d",n);
+	// Print the value of n
+	
+}
+
+
