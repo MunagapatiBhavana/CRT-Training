abd65af8903614e17277f153e2711b1f489fe84a
Bhavana <bhavanamunagapati28@gmail.com>
2025-07-07 16:24:17 +0530

    Update CC-12 The Squid Game.c

--- Week-1/Day-5/CC-12 The Squid Game.c
+++ Week-1/Day-5/CC-12 The Squid Game.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+int main(void) {
+    int t;
+    scanf("%d",&t);
+    while(t--){
+        int x,sum=0;
+        scanf("%d",&x);
+        int a[x];
+        
+        int min;
+        for(int i=0;i<x;i++){
+            scanf("%d",&a[i]);
+        }min=a[0];
+        for(int i=0;i<x;i++){
+            if(a[i]<min){
+                min=a[i];
+                
+            }sum+=a[i];
+        }printf("%d\n",sum-min);
+    }
+	return 0;
+}
