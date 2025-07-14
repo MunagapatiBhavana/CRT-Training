2ca6d55e7942e4865700491c8a87e50f726589a2
Bhavana <bhavanamunagapati28@gmail.com>
2025-07-14 18:51:40 +0530

    Update CC-29 Chef and Walking on the rectangle.c

--- Week-2/Day-10/CC-29 Chef and Walking on the rectangle.c
+++ Week-2/Day-10/CC-29 Chef and Walking on the rectangle.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    while (t--) {
+        int n, m, k;
+        scanf("%d %d %d", &n, &m, &k);
+
+        if (n == 1 && m > 2) {
+            printf("%d\n", k);
+        }
+        else if (n > 2 && m == 1) {
+            printf("%d\n", k);
+        }
+        else if (n == 1 && m <= 2) {
+            printf("0\n");
+        }
+        else if (n <= 2 && m == 1) {
+            printf("0\n");
+        }
+        else if (n >= 2 && m >= 2) {
+            if (k % 2 == 0) {
+                printf("%d\n", k / 2);
+            } else {
+                printf("%d\n", (k / 2) + 1);
+            }
+        }
+    }
+
+    return 0;
+}
 
