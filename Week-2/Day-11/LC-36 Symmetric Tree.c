f774edda5e34232ba1492541d80cc55d69146d05
Bhavana <bhavanamunagapati28@gmail.com>
2025-07-14 19:01:55 +0530

    Update LC-36 Symmetric Tree.c

--- Week-2/Day-11/LC-36 Symmetric Tree.c
+++ Week-2/Day-11/LC-36 Symmetric Tree.c
@@ -1,1 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+bool isMirror(struct TreeNode* t1, struct TreeNode* t2) {
+    if (t1 == NULL && t2 == NULL) return true;
+    if (t1 == NULL || t2 == NULL) return false;
 
+    return (t1->val == t2->val) &&
+           isMirror(t1->left, t2->right) &&
+           isMirror(t1->right, t2->left);
+}
+bool isSymmetric(struct TreeNode* root) {
+    if (root == NULL){
+        return true;
+    } 
+    return isMirror(root->left, root->right);
+    
+}
