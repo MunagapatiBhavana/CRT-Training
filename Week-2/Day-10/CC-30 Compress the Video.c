eafbbf20d8d72c985c54091fcf9fe347735f344b
Bhavana <bhavanamunagapati28@gmail.com>
2025-07-14 18:52:05 +0530

    Update CC-30 Compress the Video.c

--- Week-2/Day-10/CC-30 Compress the Video.c
+++ Week-2/Day-10/CC-30 Compress the Video.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+    scanf("%d", &t);
+    while (t--) {
+        int n, c = 0, p = 0, x;
+        scanf("%d", &n);
+        while (n--) {
+            scanf("%d", &x);
+            if (x != p) {
+                p = x;
+                c++;
+            }
+        }
+        printf("%d\n", c);
+    }
+	return 0;
+}
 
