44a8efb954c99a7af20463a088ca8d54c8f1d8f2
Bhavana <bhavanamunagapati28@gmail.com>
2025-07-14 18:54:11 +0530

    Update LC-30 Binary Tree Inorder Traversal.c

--- Week-2/Day-11/LC-30 Binary Tree Inorder Traversal.c
+++ Week-2/Day-11/LC-30 Binary Tree Inorder Traversal.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+void inorder(struct TreeNode* root, int* result, int* index) {
+    if (root == NULL) return;
+    inorder(root->left, result, index);
+    result[(*index)++] = root->val;
+    inorder(root->right, result, index);
+}
+
+int* inorderTraversal(struct TreeNode* root, int* returnSize) {
+    int* result = (int*)malloc(100 * sizeof(int));
+    *returnSize = 0;
+    inorder(root, result, returnSize);
+    return result;
+}
